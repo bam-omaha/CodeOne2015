5270bdbec2b7704f49232757ca883fb725d8064e
Austin L <pumafied@gmail.com>
2015-09-27 17:38:58 -0500

    Updates

--- setup.py
+++ setup.py
@@ -10,15 +10,17 @@ db.create_all()
 admin_user = create_user('admin', 'admin@example.com', 'password', is_admin=True)
 
 # Read in checking data
+count = 0
 with open('checkingData.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for description, category_name, amount, date in readCSV:
         category = model.Category.query.filter_by(title=category_name).first()
+        count += 1
         if category is None:
             category = model.Category(category_name)
             db.session.add(category)
             db.session.commit()
-
+        print(count)
         print("Adding entry for {} at {}".format(category_name, date))
         create_transaction(admin_user, description, amount, category, date)
 
