c957b9b58c28abb88e64f49c5fe1771c01fc15f8
Mitchell Bruckner <mbruckner807@gmail.com>
2015-09-26 20:44:01 -0500

    made grammar more robust

--- process.py
+++ process.py
@@ -3,7 +3,7 @@ import nltk
 
 def parse(text):
     cp = nltk.load_parser('thing.fcfg',cache=False)
-    thing = cp.parse(text.split())
+    thing = cp.parse(text.lower().split())
     tree = list(thing)[0]
     tree.label()['SEM']
     return " ".join(tree.label()['SEM'])
